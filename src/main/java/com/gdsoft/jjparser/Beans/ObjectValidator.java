14de9f528cd49d1f5404fc9a15db7fe294b8fca8
GDLMadushanka <gdlmadushanka@gmail.com>
2018-09-17 04:40:21 +0530

    Add number of properties validation for objects

--- src/main/java/com/gdsoft/jjparser/Beans/ObjectValidator.java
+++ src/main/java/com/gdsoft/jjparser/Beans/ObjectValidator.java
@@ -6,7 +6,6 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Set;
 
 public class ObjectValidator {
@@ -21,6 +20,8 @@ public class ObjectValidator {
 
     public static void validateObject(JsonObject object, JsonObject schema) throws ParserException {
         boolean additionalProperties = true;
+        int minimumProperties = -1;
+        int maximumProperties = -1;
         if (schema.has(REQUIRED)) {
             if (schema.get(REQUIRED).isJsonArray()) {
                 JsonArray requiredArray = schema.getAsJsonArray(REQUIRED);
@@ -38,9 +39,29 @@ public class ObjectValidator {
                 additionalProperties = DataTypeConverter.convertToBoolean(additionalPropertyString);
             }
         }
+        if (schema.has(MIN_PROPERTIES)) {
+            String minPropertiesString = schema.get(MIN_PROPERTIES).getAsString().replaceAll
+                    (ParserConstants.REGEX, "");
+            if (!minPropertiesString.isEmpty()) {
+                minimumProperties = DataTypeConverter.convertToInt(minPropertiesString);
+            }
+        }
+        if (schema.has(MAX_PROPERTIES)) {
+            String maxPropertiesString = schema.get(MAX_PROPERTIES).getAsString().replaceAll
+                    (ParserConstants.REGEX, "");
+            if (!maxPropertiesString.isEmpty()) {
+                maximumProperties = DataTypeConverter.convertToInt(maxPropertiesString);
+            }
+        }
         JsonObject schemaObject = (JsonObject) schema.get("properties");
         Set<Map.Entry<String, JsonElement>> entryInput = object.entrySet();
-
+        int numOfProperties = entryInput.size();
+        if(minimumProperties != -1 && numOfProperties < minimumProperties) {
+            throw new ParserException("Object violates the minimum number of properties constraint");
+        }
+        if(maximumProperties != -1 && numOfProperties > maximumProperties) {
+            throw new ParserException("Object violates the maximum number of properties constraint");
+        }
         if (!additionalProperties) {
             for (Map.Entry<String, JsonElement> temp : entryInput) {
                 if (!schemaObject.has(temp.getKey())) {
